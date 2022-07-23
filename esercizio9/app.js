8fc7403415dd09fc7f529fd5720524edd79fedac
Sara Esu <saraesu@gmail.com>
2022-07-23 12:12:28 +0200

    fix:esercizio9

--- esercizio9/app.js
+++ esercizio9/app.js
@@ -1,7 +1,7 @@
 const { createServer } = require('node:http');
 
 function createApp() {
-    return createServer((equest, rresponse) => {
+    return createServer((request, response) => {
         response.statusCode = 200;
 
         response.setHeader('Content-Type', 'text/html');
